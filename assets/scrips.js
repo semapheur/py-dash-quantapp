0066c1529a352b1ab8f24a8763aa25e0e5a5a4fc
semapheur <danfyll@hotmail.com>
2023-09-30 21:50:51 +0200

    Deploy

--- assets/scrips.js
+++ assets/scrips.js
@@ -20,6 +20,18 @@ window.dash_clientside = Object.assign({}, window.dash_clientside, {
   
       dialog = document.getElementById(dialog_id)
       dialog.showModal()
+
+      dialog.addEventListener('click', e => {
+        const dialogDimensions = dialog.getBoundingClientRect()
+        if (
+          e.clientX < dialogDimensions.left ||
+          e.clientX > dialogDimensions.right ||
+          e.clientY < dialogDimensions.top ||
+          e.clientY > dialogDimensions.bottom
+        ) {
+          dialog.close()
+        }
+      })
   
       return {
         data: data.data,
