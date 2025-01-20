805decb4a1441cfd2ab09750ff77e186889a6ea1
semapheur <danfyll@hotmail.com>
2025-01-20 01:56:51 +0100

    Backup

--- src/pages/tools/securities.py
+++ src/pages/tools/securities.py
@@ -105,8 +105,8 @@ def update_graph(
   frequency: float | None,
   returns: float | None,
   fees: float | None,
-  tax: float | None,
-  deduction: float | None,
+  tax_rate: float | None,
+  deduction_rate: float | None,
   years: float | None,
 ):
   if (
@@ -115,8 +115,8 @@ def update_graph(
     or frequency is None
     or returns is None
     or fees is None
-    or tax is None
-    or deduction is None
+    or tax_rate is None
+    or deduction_rate is None
     or years is None
   ):
     return no_update
@@ -126,8 +126,9 @@ def update_graph(
   steps = years * frequency + 1
   t = np.linspace(0, years, steps)
 
-  principal_array = np.full(steps, principal)
+  principals = np.full(steps, principal)
   balance = np.zeros(steps)
+  tax = np.zeros(steps)
   gain = np.zeros(steps)
   real_gain = np.zeros(steps)
 
@@ -135,23 +136,38 @@ def update_graph(
 
   annual_return = (returns - fees) / 100
   effective_periodic_return = (1 + annual_return) ** (1 / frequency) - 1
-  effective_tax = (tax - deduction) / 100
+  tax_rate = tax_rate / 100
+  deduction_rate = deduction_rate / 100
+
+  deduction_basis = principal * (1 + deduction_rate)
+  deduction = deduction_basis * deduction_rate
+
+  contribution_made_this_year = False
 
   for i in range(1, len(t)):
-    balance[i] = balance[i - 1] * (1 + effective_periodic_return)
-    if i % frequency == 0:
-      balance[i] += contribution
+    balance[i] = balance[i - 1] * (1 + effective_periodic_return) + contribution
+
+    principals[i] = principals[i - 1] + contribution
 
     if i % frequency == 0:
-      effective_tax = (tax - (deduction * (i // frequency))) / 100
+      if not contribution_made_this_year:
+        deduction_basis += contribution
+        deduction_basis *= 1 + deduction_rate
+        deduction = deduction_basis * deduction_rate
+        contribution_made_this_year = True
 
-    gain[i] = balance[i] - principal
-    real_gain[i] = gain[i] * (1 - effective_tax)
+    else:
+      if i % frequency == 0:
+        contribution_made_this_year = False
+
+    gain[i] = balance[i] - principals[i]
+    tax[i] = max(0, gain[i] - deduction) * tax_rate
+    real_gain[i] = gain[i] - tax[i]
 
   fig = go.Figure()
   fig.add_scatter(
     x=t,
-    y=principal_array,
+    y=principals,
     mode="lines",
     name="Principal",
     line_shape="hv",
@@ -167,9 +183,24 @@ def update_graph(
   )
   fig.add_scatter(
     x=t,
+    y=tax,
+    mode="lines",
+    name="Tax",
+    line_shape="hv",
+    stackgroup="balance",
+  )
+  fig.add_scatter(
+    x=t,
     y=balance,
     mode="lines",
     name="Balance",
     line_shape="hv",
+  )
+
+  fig.update_layout(
+    title="Accumulated returns",
+    xaxis_title="Years",
+    yaxis_title="Value",
+    hovermode="x unified",
   )
   return fig
